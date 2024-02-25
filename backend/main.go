634f79811ad44ad6d655775923842f1143ce1489
GigaOwO <takumagiga1118@gmail.com>
2024-02-26 00:14:41 +0900

    backend

--- backend/main.go
+++ backend/main.go
@@ -1,13 +1,28 @@
 package main
 
 import (
+	"NextGo/ent"
+	"context"
+	_ "github.com/lib/pq"
 	"github.com/gin-gonic/gin"
+	"log"
 )
 
 func main() {
 	//Ginフレームワークのデフォルトの設定を使用してルータを作成
 	router := gin.Default()
-	
+
+	//PostgreSQLに接続
+    client, err := ent.Open("postgres", "host=db port=5432 user=postgres dbname=db password=password sslmode=disable")
+ 
+ 	if err != nil {
+		log.Fatalf("failed opening connection to postgres: %v", err)
+	}
+	// Run the auto migration tool.
+	if err := client.Schema.Create(context.Background()); err != nil {
+		log.Fatalf("failed creating schema resources: %v", err)
+	}
+
 	// ルートハンドラの定義
 	router.GET("/", func(c *gin.Context) {
 		c.JSON(200, gin.H{
